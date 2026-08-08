5cbe03085c9b572dc4b595ab8106c39b2d4aa0f6
muqaddaseman66-jpg <muqaddaseman66@gmail.com>
2026-08-08 01:11:57 -0700

    react practices

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="title">Hello React | Muqaddas </h1>
+  );
+}
+
+export default App;
---
+++ vite-react/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <h1>Hello Vite + React | Muqaddas</h1>
+  )
+}
+
+export default App
